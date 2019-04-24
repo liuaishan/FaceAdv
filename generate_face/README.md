faa912f7f99bf8963dd32fa3745ff831472126c1
HBhswl <157908876@qq.com>
2019-04-24 16:48:36 +0800

    add note and readme

---
+++ generate_face/README.md
@@ -0,0 +1,17 @@
+## Generate_face
+
+#### Files included
+
+- datasets.py
+- models.py
+- pix2pix.py
+- utils.py
+
+## How to train this model
+``` python
+	python pix2pix
+```
+
+### to do list
+- something wrong when embed the cifar
+
--- generate_face/datasets.py
+++ generate_face/datasets.py
@@ -7,18 +7,28 @@ from torch.utils.data import Dataset
 from PIL import Image
 import torchvision.transforms as transforms
 
+
+"""
+   this file is to load the dataset
+"""
 class ImageDataset(Dataset):
     def __init__(self, root, transforms_=None, mode=[""]):
         self.transform = transforms.Compose(transforms_)
 
+        # load all files in to a list
         self.files = []
         for mod in mode:
             self.files += glob.glob(os.path.join(root, "%s" % mod) + "/*.*")
         self.files = sorted(self.files)
-        print("the length of files read is ", len(self.files))
 
     def __getitem__(self, index):
+        """
+          @index: which picture will be loaded
+        """
         img = self.transform(Image.open(self.files[index % len(self.files)]))
+        
+        # this is some mistake in the ImageNet dataset, which is gray
+        # so when load a gray picture and exchange it to next RGB pic
         while img.size()[0] != 3:
             index += 1
             img = self.transform(Image.open(self.files[index % len(self.files)]))
